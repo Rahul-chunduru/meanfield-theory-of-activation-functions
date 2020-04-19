f1e6e1782b646882ab7acc7c6b7ca26d66638021
Rahul-chunduru <chundururahul@gmail.com>
2020-04-19 10:10:06 +0530

    fix mnist processing

--- TF2.0/data.py
+++ TF2.0/data.py
@@ -1,4 +1,17 @@
 import tensorflow as tf
+import numpy as np
+
+def oneHotEncode(y):
+
+    num = len(y)
+    nlabels = len(np.unique(y))
+
+    one_hot_matrix = np.zeros((num, nlabels))
+
+    for i, c in enumerate(y):
+        one_hot_matrix[i][y[i] - 1] = 1
+
+    return one_hot_matrix
 
 mnist = tf.keras.datasets.mnist
 
@@ -8,4 +21,4 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 m, n, o = x_train.shape
 
 x_train = x_train.reshape(m, n * o)
-y_train = one_hot_econding(y_train, 10, 0)
+y_train = oneHotEncode(y_train)
