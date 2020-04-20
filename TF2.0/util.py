fd26f94417e6bab1da724c33d3bfcb33215fa292
Rahul-chunduru <chundururahul@gmail.com>
2020-04-20 10:21:56 +0530

    esp code fix

--- TF2.0/util.py
+++ TF2.0/util.py
@@ -9,6 +9,8 @@ import tensorflow as tf
 
 from tensorflow.python.framework import ops
 
+tf.keras.backend.set_floatx('float64')
+
 class GenericLayer(tf.keras.layers.Layer):
   def __init__(self, units, activation, name=None): 
      super(GenericLayer, self).__init__(name=name) 
@@ -22,16 +24,16 @@ class GenericLayer(tf.keras.layers.Layer):
         train = False
 
       self.w = self.add_weight( 
-        shape=([input_shape[1], self.units]), initializer='random_normal', name='w', dtype='float32')
+        shape=([input_shape[1], self.units]), initializer='random_normal', name='w', dtype='float64')
       self.beta = self.add_weight( 
-        shape=([input_shape[1], self.units]), initializer='random_normal', name='beta', dtype='float32', trainable=train)
+        shape=([self.units]), initializer='random_normal', name='beta', dtype='float64', trainable=train)
       self.b = self.add_weight( 
-        shape=([self.units]), initializer='random_normal', dtype='float32', name='b')
+        shape=([self.units]), initializer='random_normal', dtype='float64', name='b')
 
   def call(self, x): 
      y = tf.matmul(x, self.w) + self.b 
      if self.activation == 'esp':
-        y = tf.matmul(y, tf.math.sigmoid(tf.mathmul(self.beta, y)))    
+        y = tf.math.multiply(y, tf.math.sigmoid(tf.math.multiply(self.beta, y)))    
      elif self.activation == 'relu':
         y = tf.nn.relu(y)
      elif self.activation == 'sigmoid':
@@ -82,28 +84,13 @@ def eval_accuracy(y_true, y_pred, method):
 
   if method == 'logistic': #accuracy score for binary class classification
       correct = tf.equal(tf.argmax(y_pred, axis = 1), tf.argmax(y_true, axis = 1))
-      accuracy= tf.reduce_mean(tf.cast(correct, "float32"))
+      accuracy= tf.reduce_mean(tf.cast(correct, "float64"))
 
   elif method == 'regression': #r2 score
       norm= tf.reduce_mean( tf.squared_difference( y_true,tf.reduce_mean(y_true)) )
       accuracy = 1 - tf.divide( tf.reduce_mean(tf.squared_difference(y_pred, y_true)), norm)
   
   return accuracy
-
-def train(x, y, model, obj, method, num_iter, lr):
-
-  opt = tf.keras.optimizers.SGD(lr)
-
-  for epoch in range(num_iter):
-    with tf.GradientTape() as g:
-      cost = obj(model(x), y, method)
-      print("epoch", epoch, cost)
-
-    grads = g.gradient(cost, model.trainable_variables)
-
-    opt.apply_gradients(zip(grads, model.trainable_variables))
-  
-  return model
 
 #------------Hessian-------------------
 
@@ -151,23 +138,38 @@ def hessian(grads, par):
     dim = flat_grads.get_shape()[0] #get the dimensions of the flattened tensor
 
     hess = [] #list
-    print("check51")
     for i in range (dim):
 
         dg_i = tf.gradients(flat_grads[i], par) #for each element of grads evaluate the gradients
         dg_i_flat = flatten(dg_i) #flatten the resulting hessian onto a 1 d array
         hess.append(dg_i_flat) #store row by row
 
-    print("check52")
     return tf.reshape(hess,[dim, dim]) #returns the reshaped matrix
+
+def train(x, y, model, obj, method, num_iter, lr):
+
+  opt = tf.keras.optimizers.SGD(lr)
+
+  for epoch in range(num_iter):
+    with tf.GradientTape() as g:
+      cost = obj(model(x), y, method)
+      print("epoch", epoch, cost)
+
+    grads = g.gradient(cost, model.trainable_variables)
+
+    opt.apply_gradients(zip(grads, model.trainable_variables))
+  
+  return model
 
 # sample code
 
 # # model specification
-# model = DNN([128, 28, 10], ['dense', 'sigmoid', 'softmax'])
+model = DNN([128, 28, 10], ['dense', 'esp', 'softmax'])
 
 # # train
-# model = train(x_train, y_train, model, obj, 'crossEntropy', 1000, 0.1)
+model = train(x_train, y_train, model, obj, 'crossEntropy', 100, 0.1)
 
 # # evaluation
-# obj(model(xt), yt, 'crossEntropy'), eval_accuracy(model(xt), yt, 'logistic')
+obj(model(x_train), y_train, 'crossEntropy'), eval_accuracy(model(x_train), y_train, 'logistic')
+
+# Can access all the parameters using - model.trainable_variables
